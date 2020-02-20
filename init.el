a8813f7c5bb0aa7f1f843b6d1db99a31b4766e6d
Javier Sagredo <js@concordium.com>
2020-02-20 19:58:03 +0100

    Refactor of the repo

--- init.el
+++ init.el
@@ -1,63 +1,70 @@
-;; Init package
+;; Global configs
 (set-language-environment "UTF-8")
 (set-default-coding-systems 'utf-8)
-;; Solve issue with TLS
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 
-(require 'package)
-;; Add community driven repository
-(add-to-list 'package-archives (cons "melpa" "https://melpa.org/packages/") t)
-(package-initialize)
-;; Install packages in first run
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (setq packages '(helm                 ;; minibuffer autocomplete engine
-                   helm-company         ;; use helm in company
-                   helm-flycheck        ;; use helm with flycheck
-                   ace-window           ;; jump fast to other windows
-                   intero               ;; haskell-stack
-                   haskell-mode         ;; haskell
-                   use-package          ;; pkg configurations
-                   undo-tree            ;; better undo/redo
-                   neotree              ;; file tree explorer
-                   rainbow-mode         ;; colors on parenthesis
-                   multiple-cursors     ;; multiple cursors
-                   magit                ;; git
-                   indent-guide         ;; visual indentation lines
-                   exec-path-from-shell ;; get env variables
-                   doom-themes          ;; themes
-                   diminish             ;; small icons for modes
-                   dashboard            ;; starting dashboard
-                   company              ;; autocompletion
-                   projectile           ;; project management
-                   projectile-ripgrep   ;; rg
-                   helm-projectile      ;; helm + projectile
-                   lsp-ui               ;; visual help with doc powered by lsp
-                   cargo                ;; cargo
-                   racer                ;; more Rust
-                   rust-mode            ;; Rust
-                   rustic               ;; Better Rust
-                   flycheck             ;; syntax checking
-                   flycheck-rust        ;; syntax checking rust
-                   which-key            ;; learn new key commands
-                   carbon-now-sh        ;; code snapshots
-                   org-bullets          ;; UTF bullets in org mode
-                   all-the-icons        ;; pretty icons for my editor
-                   cheatsheet           ;; friendly cheatsheet
-                   deadgrep             ;; ripgrep
-                   ))
-  (mapc (lambda (x) (unless (package-installed-p x)
-                 (package-install x))) packages))
-
-(require 'use-package)
-(require 'org)
-
-(setq inits-dir "~/.emacs.d/inits/")
-(setq inits (directory-files-recursively inits-dir ".*\.org$"))
-(mapc (lambda (x) (org-babel-load-file x)) inits)
-
-;; Exclude some files from recentf
-(setq recentf-exclude (cons "~/.emacs.d/bookmarks" org-agenda-files))
-;; customize file
+;; Customize file
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
+
+(require 'package)
+;; Add melpa
+(add-to-list 'package-archives (cons "melpa" "https://melpa.org/packages/") t)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (setq package-check-signature nil)
+  (package-refresh-contents)
+  (package-install 'gnu-elpa-keyring-update)
+  (setq package-check-signature 'allow-unsigned)
+  (package-initialize)
+  (package-refresh-contents)
+  (mapc (lambda (x) (unless (package-installed-p x)
+                 (package-install x)))
+        '(
+          doom-themes
+          magit
+          projectile
+          projectile-ripgrep
+          flycheck
+          company
+          treemacs
+          treemacs-projectile
+          treemacs-magit
+          helm
+          helm-company
+          helm-flycheck
+          helm-projectile
+          ace-window
+          use-package
+          undo-tree
+          rainbow-mode
+          multiple-cursors
+          indent-guide
+          exec-path-from-shell
+          diminish
+          dashboard
+          which-key
+          org-bullets
+          deadgrep
+          sane-term
+          all-the-icons
+          cheatsheet
+          anzu
+          beacon
+          carbon-now-sh
+          ;; programming
+          lsp-mode
+          haskell-mode
+          cargo
+          racer
+          rust-mode
+          flycheck-rust)))
+
+;; Load packages installed locally
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+(load "prolog")
+(load "ghcid")
+
+(org-babel-load-file "config.org")
+
